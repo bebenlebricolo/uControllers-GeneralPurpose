5c3625b7b6f634f1484ccc196c900345c36f565b
Bebenlebricolo <bebenlebricolo@users.noreply.github.com>
2017-12-09 13:07:52 +0100

    Updated adc_tools
    Few changes, better request stack handling, resets a request to NULL when completed. 
    Simulation and µcontroller flashing proved it to work as expected with 2 sensors.

--- ADC_asynchronous_tools/Sources/adc_tools.cpp
+++ ADC_asynchronous_tools/Sources/adc_tools.cpp
@@ -1,13 +1,17 @@
 ﻿
-
 #include "adc_tools.h"
 #include <avr/io.h>
-#include "sensors.h"
+#include "Sensors.h"
 #include <stdint.h>
-#include <stddef.h>
+#include <stddef.h> // NULL pointer needs it
+
+const uint8_t max_Sensor_Requests = 4;
 
 
-
+Adc::Adc():req_iterator(0),processing_iterator(0),tot_req(0),req_full_flag(0)
+{
+	purge_requests(); // Initializing the requests table to NULL
+}
 
 void Adc::purge_requests()
 {
@@ -16,36 +20,40 @@ void Adc::purge_requests()
 		requests[i]=NULL; // Initializing to NULL
 	}
 }
-Adc::Adc():req_iterator(0),processing_iterator(0),tot_req(0),req_full_flag(0)
-{
-	purge_requests(); // Initializing the requests table to NULL
-}
 
-uint8_t Adc::add_request(Sensor *sensor)
+
+uint8_t Adc::add_request(AnalogSensor *sensor)
 {
-	if(req_full_flag == 0){
-		requests[req_iterator] = sensor; // pointer
-		req_iterator = (req_iterator + 1) % ADC_REQUEST_SIZE ;
-		if(tot_req == 0 && (ADCSRA & 1<<ADSC)==0) start_conversion();
-		tot_req++;
-		if(tot_req == ADC_REQUEST_SIZE) req_full_flag++;
-		return 1;
+	if(req_full_flag == 0){	// if pending requests array is not full
+		requests[req_iterator] = sensor; // Add sensor's adress in pending request array
+		req_iterator = (req_iterator + 1) % ADC_REQUEST_SIZE ; // increments the request iterator (next request)
+		if(tot_req == 0 && (ADCSRA & 1<<ADSC)==0) start_conversion(); // if adc is idle (no requests), start a conversion
+		tot_req++;	//	increments the number of pending requests
+		if(tot_req == ADC_REQUEST_SIZE) req_full_flag++;	// if pending requests array is full, discard future requests
+		return 1;	//	request successfully added
 	}
 	else return 0;
 }
 
+// Starts an Adc conversion (updates registers, set AdcMux channel and trigger conversion)
 void Adc::start_conversion(){
-	Sensor *sensor = requests[processing_iterator];
-	ADMUX = (ADMUX & (0b11110000)) | sensor->get_Adc_Mux();
-	if((ADCSRA & 1<<ADSC)==0)
+	AnalogSensor *sensor = requests[processing_iterator];	// Fetches the currently evaluated sensor
+	while(sensor == NULL)
+	{ processing_iterator = (processing_iterator + 1) % ADC_REQUEST_SIZE;
+		sensor = requests[processing_iterator];
+	}
+	ADMUX = (ADMUX & (0b11110000)) | sensor->get_adc_mux();	// Select conversion channel
+	if((ADCSRA & 1<<ADSC)==0)	// if adc is not busy (=> ADSC bit == 0 )
 	{
 		ADCSRA |= (1<<ADSC);	// start conversion
 	}
 }
 
+// Switches on the Adc (wakes it up!)
 void Adc::initialize()
 {
-	req_iterator = 0;
+	purge_requests();	// Purges Adc pending requests array
+	req_iterator = 0;	// starts with the first iterator
 	req_full_flag = 0;
 	tot_req = 0;
 	ADCSRA = (1<<ADEN) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
@@ -54,23 +62,79 @@ void Adc::initialize()
 	PRR &= ~(1<<PRADC); //  Switches off the power reduction bit on ADC
 }
 
+// Returns the Adc pointer
 Adc* Adc::getPointer(void)
 {
 	return this;
 }
 
+// Decreases total pending request number
 void Adc::conversion_complete()
 {
+	requests[processing_iterator] = NULL;
 	tot_req--;
 	if(tot_req <= ADC_REQ_LATCH) req_full_flag=0;
-	processing_iterator = (processing_iterator + 1) % ADC_REQUEST_SIZE ;
+	processing_iterator = (processing_iterator + 1) % (ADC_REQUEST_SIZE) ;
 	// Now it's time to handle the next conversion
-	if(tot_req != 0) start_conversion();	
+	if(tot_req > 0) start_conversion();
 }
 
 // Extracts the pointer of the currently evaluated sensor
-Sensor* Adc::get_current_sensor_id(){
+AnalogSensor* Adc::get_current_sensor_id(){
 	return requests[processing_iterator];
 }
 
+// Clears all requests of one AnalogSensor
+uint8_t Adc::clear_sensor_requests(AnalogSensor* sensor)
+{
+	uint8_t requests_removed = 0;
+	for(uint8_t i=0; i<ADC_REQUEST_SIZE;i++)
+	{
+		if(requests[i] == sensor)
+		{
+			requests[i] = NULL;
+			requests_removed++;
+		}
+	}
+	return requests_removed;
+}
 
+
+AdcHandler::AdcHandler() : adc_mux(0), tot_request_nb(0),
+max_request_nb(max_Sensor_Requests), full_flag(0){}
+
+AdcHandler::AdcHandler(uint8_t n_mux, uint8_t max_req) : adc_mux(n_mux), max_request_nb(max_req),
+tot_request_nb(0),full_flag(0){}
+// Adds an adc_request and send it to the Adc
+void AdcHandler::send_adc_request(Adc* adc,AnalogSensor* sensor){
+	if(full_flag) return;	// If we hit max_adc_req_nb earlier, discard new adc_request
+	if(adc->add_request(sensor)) tot_request_nb++;	// if request has been added correctly, increment tot_request_nb
+	if(tot_request_nb >= max_request_nb) full_flag++; // Discard future adc_requests if we hit the max req nb for this sensor
+}
+
+void AdcHandler::conversion_complete() {
+	full_flag = 0 ;
+	tot_request_nb--;
+}
+
+
+void AdcHandler::set_max_adc_req_nb(uint8_t nb) {max_request_nb = nb;}
+
+// TODO filter new mux values (verify if they're correct)
+void AdcHandler::set_adc_mux(uint8_t n_mux) { adc_mux = n_mux; }
+
+volatile uint8_t AdcHandler::get_max_req_nb() {return max_request_nb;}
+volatile uint8_t AdcHandler::get_tot_req_nb() {return tot_request_nb;}
+volatile uint8_t AdcHandler::is_full() {return full_flag;}
+volatile uint8_t AdcHandler::get_mux() {return adc_mux;}
+
+void AdcHandler::clear_adc_req(Adc* adc, AnalogSensor* sensor)
+{
+	uint8_t removed_requests = 0;
+	removed_requests = adc->clear_sensor_requests(sensor);
+	if(removed_requests)
+	{
+		tot_request_nb = 0;
+		full_flag = 0;
+	}
+}
--- ADC_asynchronous_tools/Sources/adc_tools.h
+++ ADC_asynchronous_tools/Sources/adc_tools.h
@@ -1,28 +1,69 @@
-#ifndef ADC_HEADER
+/*
+* Here is the reimplementation of adc_tools.
+* It contains an Adc class which is used to handle asynchronously 
+* Adc conversion while the main programm is running
+* 
+* Author : bebenlebricolo
+* 
+* Version |   date   |  description
+*  V 0.1   27/11/2017  First use of adc_tools (adaptation to Meteor Project)
+*  V 0.2   06/12/2017  Minor corrections. Used with AnalogSensor_test -> successfully tested!
+*
+*/
+
+
+#if !defined(ADC_HEADER) && defined(SENSORS_HEADER)
 #define ADC_HEADER
 
 #define ADC_REQUEST_SIZE 8
 #define ADC_REQ_LATCH ADC_REQUEST_SIZE/2
 
+class AnalogSensor;
+#include "Sensors.h"
 
-#include "sensors.h"
 
 class Adc{
-	public:
+public:
 	Adc();  // Constructor
-	uint8_t add_request(Sensor *sensor);
+	uint8_t add_request(AnalogSensor *sensor);
 	void initialize();
 	void conversion_complete();
-	Sensor* get_current_sensor_id();
+	AnalogSensor* get_current_sensor_id();
 	Adc* getPointer();
 	void purge_requests();
+	uint8_t clear_sensor_requests(AnalogSensor *sensor);
 	void start_conversion();
-	private:
-	Sensor *requests[ADC_REQUEST_SIZE];  // Holds the list of sensors id which have pending requests
+private:
+	AnalogSensor *requests[ADC_REQUEST_SIZE];  // Holds the list of sensors id which have pending requests
 	volatile uint8_t req_iterator;    // Used to store the requests
 	volatile uint8_t processing_iterator; // Used to process each request
 	volatile uint8_t tot_req;         // Total request counter
 	volatile uint8_t req_full_flag;   // Used to know if request list is full
 };
+
+// Class which is used to handle adc operations of sensors (Gimbals & pots)
+class AdcHandler {
+public:
+	AdcHandler();
+	AdcHandler(uint8_t n_mux , uint8_t max_req);
+	
+	void send_adc_request(Adc* adc, AnalogSensor* sensor);
+	void set_max_adc_req_nb(uint8_t nb);
+	void set_adc_mux(uint8_t n_mux);
+	void clear_adc_req(Adc* adc, AnalogSensor* sensor);
+	
+	volatile uint8_t get_mux();
+	volatile uint8_t get_tot_req_nb();
+	volatile uint8_t get_max_req_nb();
+	volatile uint8_t is_full();
+
+	void conversion_complete();	
+private:
+	volatile uint8_t adc_mux;	// stores the mux adress for channel reading (multpiplexing)
+	volatile uint8_t tot_request_nb; // Stores total request number (currently executed)
+	volatile uint8_t max_request_nb; // Maximum requests number that could be handled by the sensor
+	volatile uint8_t full_flag;	// Used to track if the sensor has sent all of its available requests
+	
+	};
 
 #endif
